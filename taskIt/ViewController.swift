d7496d70e25bc647c27b37f70e73059ee9539f64
Andrew Morrison <morro141@gmail.com>
2016-03-19 23:12:53 -0300

    Initial Commit

---
+++ taskIt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  taskIt
+//
+//  Created by Andrew Morrison on 2016-03-19.
+//  Copyright © 2016 Andrew Morrison. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
